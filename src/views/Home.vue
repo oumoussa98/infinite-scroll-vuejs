8387447450d35a37fee3a895c1c2c08e7ab25f7f
abdelouahed <doonabdo98@gmail.com>
2020-06-09 21:11:32 +0000

    add javascript code fo getting todos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <v-container class="home">
     <v-row>
-   <template>
   <v-card
     max-width="344"
     class="mx-auto"
@@ -45,7 +44,6 @@
       </v-btn>
     </v-card-actions>
   </v-card>
-</template>
     </v-row>
   </v-container>
 </template>
@@ -55,56 +53,19 @@
     name: 'Home',
 
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
+      todos:[],
     }),
+     methods: {
+                getExperience: function() {
+                    axios.get('http://localhost:8000/cvs/experiences') 
+                    .then(response => {
+                      this.todos = response.data ;
+                    })
+                    .catch(error =>{
+                       return console.log('error:',error);
+                    })
+
+                },
+            },
   }
 </script>
