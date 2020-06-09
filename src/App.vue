ba0ca10c36c56134dab3960516fdab6b8f714785
abdelouahed <doonabdo98@gmail.com>
2020-06-09 19:41:28 +0000

    remove hello world component from about.vue

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
-      dark
+      color="white"
     >
       <div class="d-flex align-center">
         <v-img
@@ -20,16 +19,36 @@
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-light.png"
           width="100"
         />
       </div>
 
-      <v-spacer></v-spacer>
+      <v-spacer>
+        <router-link to="/">
+        <v-btn
+        color="black"
+        text
+        class="ml-4"
+        >
+        <span class="mr-2">Home</span>
+        </v-btn>
+        </router-link>
+        <router-link to="/about">
+        <v-btn
+        color="black"
+        text
+        >
+        <span class="mr-2">About</span>
+        </v-btn>
+        </router-link>
+      </v-spacer>
 
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
+        color="black"
         text
       >
         <span class="mr-2">Latest Release</span>
@@ -37,20 +56,18 @@
       </v-btn>
     </v-app-bar>
 
-    <v-content>
-      <HelloWorld/>
+    <v-content light >
+      
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
